02d4398ab18e56ecf5af75191ab9bf8fe362fa88
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-05-27 12:02:31 +0530

    Added test for marsrover class

--- src/test/java/mars/rover/MarsRoverTest.java
+++ src/test/java/mars/rover/MarsRoverTest.java
@@ -1,8 +1,33 @@
 package mars.rover;
 
+import exception.InvalidCoordinatesExceptions;
+import exception.NoInstructionFoundException;
 import org.junit.jupiter.api.Test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class MarsRoverTest {
+
+    @Test
+    public void shouldTurnLeftOnInstructionOfTuringLeft() throws InvalidCoordinatesExceptions, NoInstructionFoundException {
+        Plateau plateau = new Plateau(4,4, new MarsRover(1,4,Direction.N));
+        MarsRover rover = plateau.moveRover("L");
+
+        assertEquals("1 4 W",rover.finalPositionOfRover());
+    }
+
+    @Test
+    public void shouldTurnRightOnInstructionOfTuringRight() throws InvalidCoordinatesExceptions, NoInstructionFoundException {
+        Plateau plateau = new Plateau(4,4,new MarsRover(2,2,Direction.N));
+        MarsRover rover = plateau.moveRover("R");
+
+        assertEquals("2 2 E", rover.finalPositionOfRover());
+    }
+
+    @Test
+    public void shouldMoveOneStepOnInstructionToMove(){
+
+    }
 
 
 }
