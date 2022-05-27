6d8478975949ede628e5cfed4dca10b634f539f7
Samriddhi <agrawalsamriddhi83@gmail.com>
2022-05-27 11:12:45 +0530

    Added test for direction

---
+++ src/test/java/mars/rover/DirectionTest.java
@@ -0,0 +1,49 @@
+package mars.rover;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class DirectionTest {
+
+    @Test
+    public void shouldCheckIfNorthIsInLeftOfEast(){
+        assertEquals(Direction.E.getNextLeftDirection(),Direction.N);
+    }
+
+    @Test
+    public void shouldCheckIfSouthIsInRightOfEast(){
+        assertEquals(Direction.E.getNextRightDirection(),Direction.S);
+    }
+
+    @Test
+    public void shouldCheckIfNorthIsInRightOfWest(){
+        assertEquals(Direction.W.getNextRightDirection(),Direction.N);
+    }
+
+    @Test
+    public void shouldCheckIfSouthIsInLeftOfWest(){
+        assertEquals(Direction.W.getNextLeftDirection(),Direction.S);
+    }
+
+    @Test
+    public void shouldCheckIfWestIsInLeftOfNorth(){
+        assertEquals(Direction.N.getNextLeftDirection(),Direction.W);
+    }
+
+    @Test
+    public void shouldCheckIfEastIsInRightOfSouth(){
+        assertEquals(Direction.N.getNextRightDirection(),Direction.E);
+    }
+
+    @Test
+    public void shouldCheckIfWestIsInRightOfNorth(){
+        assertEquals(Direction.S.getNextRightDirection(),Direction.W);
+    }
+
+    @Test
+    public void shouldCheckIfEastIsInLeftOfSouth(){
+        assertEquals(Direction.S.getNextLeftDirection(),Direction.E);
+    }
+
+}
